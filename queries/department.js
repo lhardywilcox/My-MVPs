f6970720561bafd90012d7a1e963ac37fd7d5396
LHardyWilcox <motacycaryda@mac.com>
2023-03-21 20:41:51 -0700

    code updates

--- queries/department.js
+++ queries/department.js
@@ -1,6 +1,12 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const ctable = require('console.table');
 
 
-
-db.query('SELECT * from department', function (err, results) {
+function listDepartment() {
+  db.query('SELECT * from department', function (err, results) {
     console.log(results);
   });
+};
+
+module.exports = listDepartment;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 
 // Import and require mysql2, inquirer and console.table
+const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const ctable = require('console.table');
+const listDepartment = require('./queries/department');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -17,16 +25,44 @@ const db = mysql.createConnection(
 );
 
 // Inquirer menu
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'menu',
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
-    }
-  ]);
+const startMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menu',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+      }
+    ])
+
+    .then((answers) => {
+      if (answers.choices[0]) {
+        return listDepartment()
+      }
+      /*   if(answers.choice[1]) {
+  
+        }
+        if (answers.choice[2]) {
+  
+        }
+        if (answers.choice[3]) {
+          
+        }
+        if (answers.choice[4]) {
+          
+        }
+        if (answers.choice[5]) {
+          
+        }
+        if (answers.choice[6]) {
+          
+        } */
+    });
+};
 // How do I make the inquirer prompt function like a menu?
+
+startMenu();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
